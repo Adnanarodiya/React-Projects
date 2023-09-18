182f283e8b0ddde1758400bd00c74661bdd5455c
Adnanarodiya <aiarodiya07@gmail.com>
2023-09-18 12:12:10 +0530

    made router and some debug

--- demoWeb/src/main.jsx
+++ demoWeb/src/main.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />,
+      <Route path="about" element={<About />} />,
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
