e8aca2404f463814b2e81882a39351b95c8db6af
Adnanarodiya <aiarodiya07@gmail.com>
2023-10-02 15:37:48 +0530

    initialization of Blog Project

---
+++ Blog/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-blue-600 text-white p-4 text-3xl font-medium italic">
+        adnan
+      </div>
+    </>
+  );
+}
+
+export default App;
