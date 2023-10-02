e771a808daf86cda5b57ddf63c01dc0d44881116
Adnanarodiya <aiarodiya07@gmail.com>
2023-10-02 16:48:06 +0530

    auth sevice code completed

---
+++ Blog/src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import conf from "../conf/conf";
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  clien = new Client();
+  account;
+
+  constructor() {
+    this.clien.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId);
+    this.account = new Account(this.clien);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        // call another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
