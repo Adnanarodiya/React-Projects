865ae13b503f36763b7dc24e85f4d9085b941718
Adnanarodiya <aiarodiya07@gmail.com>
2023-10-05 19:01:25 +0530

    RTE and posfrom created

--- Blog/src/components/index.js
+++ Blog/src/components/index.js
@@ -4,5 +4,6 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Header from "./Header/Header";
 import Input from "./Input";
+import Rte from "./Rte";
 
-export { Footer, Container, Logo, LogoutBtn, Header, Input };
+export { Footer, Container, Logo, LogoutBtn, Header, Input, Rte };
